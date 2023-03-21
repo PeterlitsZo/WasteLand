1f6ddf0698b6b79dbc3da8703c650544540cce8f
Peterlits Zo <peterlitszo@gmail.com>
2023-03-21 20:16:11 +0800

    style: Replace original code with `try_or_return_error` macro
    Define macro `try_or_return_error` and use it to replace the original
    code in order to make the code more human-readable. This change does
    not add new functionality but improves the code style.
    
    The `try_or_return_error` macro simplifies error handling and reduces
    the amount of boilerplate code. By using this macro, we can avoid
    unnecessary `match` statements and improve the overall readability of
    the code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::{fs, str, path::{Path, PathBuf}};
 use sha256::digest;
+use std::{fs, path::PathBuf, str};
 
 pub struct Database {
     database_path: PathBuf,
@@ -10,9 +10,26 @@ pub struct Error {
     message: String,
 }
 
+impl Error {
+    fn new(message: &str) -> Self {
+        Error {
+            message: message.to_string(),
+        }
+    }
+}
+
+macro_rules! try_or_return_error {
+    ($result:expr, $message_prefix:expr) => {
+        match $result {
+            Ok(r) => r,
+            Err(e) => return Err(Error::new(&format!("{}: {}", $message_prefix, e))),
+        }
+    };
+}
+
 impl Database {
     fn gen_waste_hash(data: &[u8]) -> String {
-        return digest(data)
+        digest(data)
     }
 
     fn waste_path(&self) -> PathBuf {
@@ -24,72 +41,62 @@ impl Database {
     }
 
     pub fn create(database_path: &str) -> Result<Database, Error> {
-        let database = Database{ database_path: PathBuf::from(database_path) };
+        let database = Database {
+            database_path: PathBuf::from(database_path),
+        };
 
-        match fs::create_dir_all(database.waste_path()) {
-            Ok(_) => (),
-            Err(e) => {
-                return Err(Error {
-                    message: format!(
-                        "create database directory {}: {}",
-                        database.waste_path().display(), e
-                    ),
-                })
-            }
-        }
+        try_or_return_error!(
+            fs::create_dir_all(&database.database_path),
+            format!("create database directory {:?}", database.database_path)
+        );
+        try_or_return_error!(
+            fs::create_dir(database.waste_path()),
+            format!("create waste directory {:?}", database.waste_path())
+        );
+        try_or_return_error!(
+            fs::write(database.version_path(), b"0"),
+            "create version file"
+        );
 
-        match fs::write(database.version_path(), b"0") {
-            Ok(_) => (),
-            Err(e) => return Err(Error {
-                message: format!("create version file: {}", e.to_string())
-            })
+        Ok(database)
+    }
+
+    pub fn open(database_path: &str) -> Result<Database, Error> {
+        let database = Database {
+            database_path: PathBuf::from(database_path),
+        };
+
+        let data = try_or_return_error!(fs::read(database.version_path()), "read version file");
+
+        if data != b"0" {
+            return Err(Error::new(&format!(
+                "unexcepted version {}",
+                String::from_utf8_lossy(&data)
+            )));
         }
 
         Ok(database)
     }
 
-    pub fn open(database_name: &str) -> Result<Database, Error> {
-        let data = match fs::read(format!("{}/version", database_name)) {
-            Ok(data) => data,
-            Err(e) => return Err(Error { message: format!("read version file: {}", e.to_string()) })
-        };
-
-        if data != b"0" {
-            return Err(Error { message: format!("unexcepted version {}", String::from_utf8_lossy(&data)) })
-        }
-
-        Ok(Database {
-            database_path: PathBuf::from(database_name),
-        })
-    }
-
     pub fn put(&self, data: &[u8]) -> Result<String, Error> {
         let hash = Self::gen_waste_hash(data);
         let waste_path = self.waste_path().join(&hash);
-        match fs::write(waste_path, data) {
-            Ok(_) => Ok(hash),
-            Err(e) => Err(Error{ message: format!("write to file: {}", e) })
-        }
+        try_or_return_error!(fs::write(waste_path, data), "write to file");
+        Ok(hash)
     }
 
     pub fn get(&self, hash: &str) -> Result<Vec<u8>, Error> {
         let waste_path = self.waste_path().join(hash);
-        match fs::read(waste_path) {
-            Ok(data) => Ok(data),
-            Err(e) => Err(Error{ message: format!("read from file: {}", e) })
-        }
+        let data = try_or_return_error!(fs::read(waste_path), "read from file");
+        Ok(data)
     }
 
     pub fn drop(self) -> Result<(), Error> {
-        match fs::remove_dir_all(&self.database_path) {
-            Ok(()) => Ok(()),
-            Err(e) => Err(Error {
-                message: format!(
-                    "remove directory {}: {}",
-                    &self.database_path.display(), e
-                ),
-            }),
-        }
+        try_or_return_error!(
+            fs::remove_dir_all(&self.database_path),
+            format!("remove directory {}", &self.database_path.display())
+        );
+        Ok(())
     }
 }
 
@@ -103,7 +110,7 @@ mod tests {
         match fs::remove_dir_all(database_name) {
             Err(e) if e.kind() != ErrorKind::NotFound => {
                 panic!("{}", e)
-            },
+            }
             _ => (),
         }
     }

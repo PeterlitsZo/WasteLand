e23317be40e975ccd090a84e876bb4cb6a5feb10
Peterlits Zo <peterlitszo@gmail.com>
2023-03-21 17:34:40 +0800

    chore: Add benchmark

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,23 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use waste_island::Database;
+
+// The benchmark in my computer: 221.39 ms.
+fn benchmark(c: &mut Criterion) {
+    c.bench_function("benchmark", |b| {
+        b.iter(|| {
+            let database = Database::create("/tmp/waste_island.benchmark/")
+                .unwrap();
+            for i in 0..1024 {
+                let content = format!("{} {}", i, "just for test".repeat(1024));
+                let hash = database.put(content.as_bytes()).unwrap();
+                for i in 0..99 {
+                    database.get(&hash).unwrap();
+                }
+            }
+            database.drop().unwrap();
+        })
+    });
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
